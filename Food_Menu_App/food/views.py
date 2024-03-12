350c5fd5b74624c65533dd6e0264204b6bd22a13
Hemanth Gaddipati <gaddipatihemanth@gmail.com>
2024-03-12 02:28:50 -0500

    section7 complete

--- Food_Menu_App/food/views.py
+++ Food_Menu_App/food/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
 
 # Create your views here.
 def index(request):
     queryset = Item.objects.all()
-    return HttpResponse(queryset)
+    context = {
+        'queryset': queryset,
+    }
+    return render(request, 'food/index.html', context)
 
 def item(request):
     return HttpResponse("<h1>This is an item view!!!</h1>")
+
+def detail(request, item_id):
+    item = Item.objects.get(pk = item_id)
+    context = {
+        'item': item,
+    }
+    return render(request, 'food/detail.html', context)
